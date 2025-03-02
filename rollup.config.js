541e956f8dc4741b1ae80d3de878a479c04e7b25
ShayC0derBui <sinhaprem636@gmail.com>
2025-03-02 14:42:40 +0530

    Update build configuration to output bundled files to 'dist' directory and add build script to package.json

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ export default {
   // Use your main entry point file that imports the other modules.
   input: "static/js/main.js",
   output: {
-    file: "static/bundle.js", // bundled output file
+    file: "dist/bundle.js", // bundled output file
     format: "iife", // Immediately Invoked Function Expression, works directly in browsers.
     name: "MyApp", // Global variable name for your bundle.
     sourcemap: false,
